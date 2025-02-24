f79390b6a9f6a930fe0226cb6a613e2b08d40c58
Mykola Petrenko <mykola.p@awery.aero>
2025-02-24 21:14:44 +0000

    add pieces

--- src/Controller/AwbController.php
+++ src/Controller/AwbController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Entity\Awb;
+use App\Entity\Piece;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -29,6 +30,7 @@ class AwbController extends AbstractController
     #[Route('/api/updateAwb', name: 'api_update_awb', methods: ['POST'])]
     public function updateAwb(Request $request):JsonResponse
     {
+        $id = 0;
         $requestData = json_decode($request->getContent(), true);
         if (!empty($requestData['id'])) {
             $setting = $this->em->getRepository(Awb::class)->find($requestData['id']);
@@ -37,13 +39,48 @@ class AwbController extends AbstractController
         }
         if (!empty($setting)) {
             foreach ($requestData as $key => $value) {
-                if ($key == 'id') {
+                if ($key == 'id' || $key == 'pieces') {
                     continue;
                 }
                 $setting->{$key} = $value;
             }
             if (empty($requestData['id'])) {
                 $this->em->persist($setting);
+            }
+            $this->em->flush();
+            $id = $setting->getId();
+        }
+        if (!empty($id) && !empty($requestData['pieces'])) {
+            foreach ($requestData['pieces'] as $piece) {
+                $piece['awb_id'] = $id;
+                $this->updatePiece($piece);
+            }
+        }
+        return new JsonResponse(['status' => 'success', 'id' => $id]);
+    }
+
+    #[Route('/api/getPieces', name: 'api_get_pieces', methods: ['GET'])]
+    public function getPieces(Request $request):JsonResponse
+    {
+        return new JsonResponse($this->em->getRepository(Piece::class)->findAll());
+    }
+
+    protected function updatePiece($piece)
+    {
+        if (!empty($piece['id'])) {
+            $Piece = $this->em->getRepository(Piece::class)->find($piece['id']);
+        } else {
+            $Piece = new Piece();
+        }
+        if (!empty($Piece)) {
+            foreach ($piece as $key => $value) {
+                if ($key == 'id') {
+                    continue;
+                }
+                $Piece->{$key} = $value;
+            }
+            if (empty($piece['id'])) {
+                $this->em->persist($Piece);
             }
             $this->em->flush();
         }
