71cbbf6d6e5567de81cd6d9ee105809abc440120
Mykola Petrenko <mykola.p@awery.aero>
2025-02-24 21:26:28 +0000

    events new structure

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -12,17 +12,43 @@ class Event
     #[ORM\Column]
     public ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    public ?string $one_record_id = null;
     #[ORM\Column]
     public ?int $message_id = null;
 
+    #[ORM\Column]
+    public ?int $awb_id = null;
+
     #[ORM\Column(length: 255)]
     public ?string $short = null;
+
+    #[ORM\Column(length: 255)]
+    public ?string $type = null;
     
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     public ?\DateTime $date = null;
 
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    public ?\DateTime $dateAction = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    public ?\DateTime $dateCreate = null;
+
     #[ORM\Column(length: 255)]
     public ?string $subject = null;
+
+    #[ORM\Column(length: 255)]
+    public ?string $location = null;
+
+    #[ORM\Column]
+    public ?int $qty = null;
+
+    #[ORM\Column(length: 255)]
+    public ?string $weight = null;
+
+    #[ORM\Column(length: 255)]
+    public ?string $text = null;
 
     #[ORM\Column(length: 255)]
     public ?string $from = null;
