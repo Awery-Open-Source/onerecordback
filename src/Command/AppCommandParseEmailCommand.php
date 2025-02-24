ec592d097230017baadf360c82119dd65e18bb17
serega <serega@notan.ua>
2025-02-24 19:16:13 +0000

    gmail

--- src/Command/AppCommandParseEmailCommand.php
+++ src/Command/AppCommandParseEmailCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use App\Controller\GMailController;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -9,6 +10,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\HttpFoundation\Request;
 
 #[AsCommand(
     name: 'AppCommandParseEmail',
@@ -31,14 +33,40 @@ class AppCommandParseEmailCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
+        $io->note("Hello, Mykola");
+        $controller = new GMailController();
+        try {
+            $mailsList = array_filter(json_decode(
+                $controller->getMails(new Request([]))->getContent(),
+                true
+            )['data_list']['messages'] ?? [], fn($mail) => strtotime($mail['date']) < (time() - 60));
 
-        if ($input->getOption('option1')) {
-            // ...
+            foreach ($mailsList as &$email) {
+                try {
+                    $request = new Request();
+                    $request->initialize(
+                        $request->query->all(),
+                        $request->request->all(),
+                        $request->attributes->all(),
+                        $request->cookies->all(),
+                        $request->files->all(),
+                        $request->server->all(),
+                        ['email_id' => $email['id']]
+                    );
+                    $email = json_decode($controller->getMailDetails($request)->getContent(), true)['data_list']['message'];
+                } catch (\Exception $e) {
+                    var_dump($e->getMessage());
+                    die;
+                }
+            }
+            unset($email);
+
+            //HERE WE CAN DO ANYTHING WITH $mailsList
+            //$mailsList[X]['body'] - body of email
+            $io->note(count($mailsList));
+        } catch (\Doctrine\DBAL\Exception|Exception|\Exception $e) {
+            $io->note($e->getMessage());
         }
 
         $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
