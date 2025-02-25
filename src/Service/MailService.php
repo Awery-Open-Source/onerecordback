324c1c74022f5219032b64d018c2fdc6d58961e2
Mykola Petrenko <mykola.p@awery.aero>
2025-02-25 11:09:27 +0000

    fix

--- src/Service/MailService.php
+++ src/Service/MailService.php
@@ -114,5 +114,27 @@ class MailService
 
         $response = json_decode(curl_exec($curl));
 
+        $headers = $this->getHeaders($response);
+
+        $event->one_record_id = $headers['Location']??null;
+
+        $this->entityManager->flush();
+
+    }
+
+    private function getHeaders($response): array
+    {
+        $headers = array();
+        $header_text = substr($response, 0, strpos($response, "\r\n\r\n"));
+        foreach (explode("\r\n", $header_text) as $i => $line) {
+            if ($i === 0) {
+                $headers['http_code'] = $line;
+            } else {
+                list ($key, $value) = explode(': ', $line);
+
+                $headers[$key] = $value;
+            }
+        }
+        return $headers;
     }
 }
